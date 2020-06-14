d260a0c393db864dd11e305b08359198e920da15
NeethaC <37862946+NeethaC@users.noreply.github.com>
2020-06-14 15:27:31 +0530

    Update AwsElasticBeanstalkExample2Application.java

--- src/main/java/com/techprimers/aws/AwsElasticBeanstalkExample2Application.java
+++ src/main/java/com/techprimers/aws/AwsElasticBeanstalkExample2Application.java
@@ -2,9 +2,10 @@ package com.techprimers.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AwsElasticBeanstalkExample2Application  {
+public class AwsElasticBeanstalkExample2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsElasticBeanstalkExample2Application.class, args);
